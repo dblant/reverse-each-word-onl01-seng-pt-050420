e4fe7e8d09f3f68b135ce555d770b50db54a1de8
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 01:43:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ arg = ["Hello", "there", "and", "how", "are", "you?"]
 def reverse_each_word(arg)
   backwards_arg = []
   
-  arg.collect do |word| 
+  arg.each do |word| 
    backwards_arg << "#{word}"
   
  end
